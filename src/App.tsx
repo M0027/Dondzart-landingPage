a151e29f78e917e98a011a9e4b0c296495bfb901
Mauro <mauroassiscumbane3@gmail.com>
2025-09-06 06:24:14 -0700

    modificaoes finais

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
 import Modal from './components/Modal';
 import EnrollmentForm from './components/EnrollmentForm';
-import WhatsappBotton from './components/whatshapBottom';
+import WhatsappBotton from './components/WhatshapBottom';
 
 function App() {
   // const [count, setCount] = useState(0)
